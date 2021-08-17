1ed84042ccf8b6ce6470ca3bf326446168f9818a
mohammadabd0 <mohammadabdo518@gmail.com>
2021-08-17 11:35:46 +0300

    newupdate

--- app.js
+++ app.js
@@ -118,42 +118,42 @@ function clickHandler(event) {
     }
 }
 
-// function chartRender() {
-//     let ctx = document.getElementById('myChart').getContext('2d');
-//     let myChart = new Chart(ctx, {
-//         type : 'bar',
-//         data: {
-//             labels: busImages,
-//             datasets: [{
-//                 label: '# of Votes',
-//                 data: votes,
-//                 backgroundColor: [
-//                     'rgba(255, 99, 132, 0.2)'
-//                 ],
-//                 borderColor: [
-//                     'rgba(255, 99, 132, 1)'
-//                 ],
-//                 borderWidth: 1
-//             }, {
-//                 label: '# of views',
-//                 data: views,
-//                 backgroundColor: [
-//                     'rgba(54, 162, 235, 0.2)'
-//                 ],
-//                 borderColor: [
-//                     'rgba(54, 162, 235, 1)'
-//                 ],
-//                 borderWidth: 1
+function chartRender() {
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let myChart = new Chart(ctx, {
+        type : 'bar',
+        data: {
+            labels: busImages,
+            datasets: [{
+                label: '# of Votes',
+                data: votes,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: '# of views',
+                data: views,
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
 
-//             }]
-//         },
-//         options: {
-//             scales: {
-//                 y: {
-//                     beginAtZero: true
-//                 }
-//             }
-//         }
-//     });
-// }
-// chartRender();
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+chartRender();
